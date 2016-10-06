5962856942fef1b2422bb099b07c1f47df702f71
Leos Pol <lpol@redhat.com>
2016-10-06 09:24:59 +0200

    start removed

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while [ true ]
-do 
-    ./nextaction.py -a 6303454664686af10cc7f8ccaaed30f58b11143b -l next --parallel_suffix ":" --serial_suffix "." --hide_future 3
-    sleep 1m
-done
